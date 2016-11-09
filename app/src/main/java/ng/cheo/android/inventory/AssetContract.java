b8936f163c81308be53144480e6f59a6f32cbe27
Mickey Cheong <mickey@cheo.ng>
2016-11-10 07:39:05 +0800

    Fix issue of quantity sold column name

--- app/src/main/java/ng/cheo/android/inventory/AssetContract.java
+++ app/src/main/java/ng/cheo/android/inventory/AssetContract.java
@@ -32,7 +32,7 @@ public final class AssetContract {
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_PRICE = "price";
         public static final String COLUMN_QUANTITY = "quantity";
-        public static final String COLUMN_QUANTITY_SOLD = "quantity";
+        public static final String COLUMN_QUANTITY_SOLD = "quantity_sold";
         public static final String COLUMN_IMAGE = "image";
     }
 }
